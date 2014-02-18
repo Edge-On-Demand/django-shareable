1a2d5a667fbd928eba157864faaf0f87bef6f947
chris <chris@coronis>
2014-02-18 14:58:14 -0500

    Updated setup.

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
-import os
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
 import django_social_share
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    
 setup(
     name = "django-social-share",
     version = django_social_share.__version__,
     description = "Templatetags for 'tweet this' and 'share on facebook'",
-    long_description = read('README.rst'),
+    packages = find_packages(),
+    package_data = {
+        'django_social_share': [
+            'templates/*.*',
+            'templates/*/*.*',
+            'templates/*/*/*.*',
+            'static/*.*',
+            'static/*/*.*',
+            'static/*/*/*.*',
+        ],
+    },
     url = 'https://github.com/chrisspen/django-social-share',
     license = 'MIT',
     author = 'Chris Spencer',
     author_email = 'chrisspen@gmail.com',
-    packages = find_packages(exclude=['tests']),
-    include_package_data=True,
     classifiers = [
         'Development Status :: 3 - Alpha',
-        'Environment :: Console',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: LGPL License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    install_requires = [],
-    test_suite = "nose.collector",
+    zip_safe = False,
+    install_requires = ['Django>=1.4.0'],
 )
