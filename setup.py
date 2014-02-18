594ab5419544e1be1bdbc05316e429b6a14ae4b6
chris <chris@coronis>
2014-02-18 15:10:57 -0500

    Updated license classifier.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: LGPL License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
